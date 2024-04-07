abcd186ee671f186ac6476f8ce0aca5de43199e5
SAHAL, QUENTIN <qsahal@bouyguestelecom.fr>
2024-04-07 11:11:21 +0200

    Improve dx

--- src/tests/Title.spec.tsx
+++ src/tests/Title.spec.tsx
@@ -1,6 +1,6 @@
 import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
-import Title from '../Title/Title';
+import { Title } from '../Title';
 
 describe('App', () => {
   it('renders the App component', () => {
