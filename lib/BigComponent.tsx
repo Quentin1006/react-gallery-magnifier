c2b3ef4e6d80df180630f8f87a1b99670b05a9c3
quentin.sahal <quentin.sahal@gmail.com>
2024-05-25 11:07:18 +0200

    add nextjs example

--- lib/BigComponent.tsx
+++ lib/BigComponent.tsx
@@ -3,8 +3,9 @@ import lodash from 'lodash-es';
 
 export const BigComponent = () => {
   const now = moment();
-  const a = { a: 1 };
+  const a = { a: 1, b: 2 };
   const aa = lodash.get(a, 'a.a');
+  const b = lodash.get(a, 'a.b');
 
-  return { a, aa, now };
+  return { a, aa, b, now };
 };
