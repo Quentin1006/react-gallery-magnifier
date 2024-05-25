d2eb5eabd04c624db328177bf253c532791f1225
quentin.sahal <quentin.sahal@gmail.com>
2024-05-25 10:13:07 +0200

    remove big component because of incompat with nextjs

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,5 +1,4 @@
 export { Gallery, type GalleryImage } from './Gallery';
 export { ImageMagnifier } from './ImageMagnifier';
-export { BigComponent } from './BigComponent';
 export { version } from './version';
 export * from './Title';
